82da52bd49cedae48185819cc4af79287c886077
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2021-12-02 02:11:40 +0700

    Update sangmata.py

--- EmikoRobot/modules/sangmata.py
+++ EmikoRobot/modules/sangmata.py
@@ -30,7 +30,7 @@ async def lastname(steal):
                 response = await conv.get_response()
             except YouBlockedUserError:
                 await steal.reply(
-                    "```Error, report to @kenbotsupport```"
+                    "```Error, report to @flicksrobotsupport```"
                 )
                 return
             if r.text.startswith("Name"):
