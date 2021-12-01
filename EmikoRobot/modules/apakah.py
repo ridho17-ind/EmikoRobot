0bbc433b3ef5c31f5272e3c881cea806d09dea28
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2021-12-01 23:09:54 +0700

    Update apakah.py

--- EmikoRobot/modules/apakah.py
+++ EmikoRobot/modules/apakah.py
@@ -2,17 +2,18 @@ import random
 from EmikoRobot.events import register
 from EmikoRobot import telethn
 
-APAKAH_STRING = ["Iya", 
-                 "Tidak", 
-                 "Mungkin", 
-                 "Mungkin Tidak", 
-                 "Bisa jadi", 
-                 "Mungkin Tidak",
-                 "Tidak Mungkin",
-                 "YNTKTS",
-                 "Pala bapak kau pecah",
-                 "Apa iya?",
-                 "Tanya aja sama mamak kau tu pler"
+APAKAH_STRING = ["Iya 😅", 
+                 "Tidak 🤦🏻", 
+                 "Mungkin 🗿", 
+                 "Mungkin Tidak 🤭", 
+                 "Bisa Jadi 🤣", 
+                 "Mungkin Iya 🤔",
+                 "Tidak Mungkin 😏",
+                 "YNTKTS 😐",
+                 "Pala Bapak Kau Pecah Anj 😡",
+                 "Apa iya? 😒",
+                 "Tanya Aja Sama Mamak Kau Tuh Puki 😑"
+                 "Betul 😁",
                  ]
 
 
@@ -20,6 +21,6 @@ APAKAH_STRING = ["Iya",
 async def apakah(event):
     quew = event.pattern_match.group(1)
     if not quew:
-        await event.reply('Berikan saya pertanyaan 😐')
+        await event.reply('Berikan Saya Pertanyaan Tolol 😐')
         return
     await event.reply(random.choice(APAKAH_STRING))
